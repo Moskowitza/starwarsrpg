0e9dc131fd8a26a2f538e7d59b9415db74650c67
moskowitza <moskowitzaaron@mac.com>
2019-03-30 08:14:57 -0400

    remaining characters array

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,3 @@
-console.log('main');
 // create characters each character has Health Points`, `Attack Power` and `Counter Attack Power`.
 const characters = [
   {
@@ -42,24 +41,24 @@ const heroDiv = document.getElementById('hero');
 const defenderDiv = document.getElementById('defender');
 const attackBtn = document.getElementById('attack');
 // Loop through the characters and add them to CharactersDiv
-
 let heroNode; // the player HTML card
 let defenderNode;
-let hero; // the object
+let hero; // the hero object
 let defender;
-
+let remainingCharacters = characters;
 function select(e) {
-  console.log(e.target.parentNode.id);
   const selectedName = e.target.parentNode.id;
   // console.log(selectedName);
   if (!hero) {
     // if no hero, make one
     hero = characters.find(character => character.name === selectedName);
     console.log(hero);
+    remainingCharacters = remainingCharacters.filter(char => char !== hero);
   } else if (!defender) {
     // if there is a hero but no defender..
     defender = characters.find(character => character.name === selectedName);
     console.log(defender);
+    remainingCharacters = remainingCharacters.filter(char => char !== defender);
   }
   if (!heroNode) {
     // 1) if no heroNode, get it from charactersDiv
@@ -96,15 +95,24 @@ function battle() {
   // Assign new damage to each fighter
   hero.takeDamage();
   defender.takeDamage();
-
-  // WE need to update the DOM
-  heroDiv.innerHTML = '';
-  // update heroNode with new hps
-  heroDiv.appendChild(createCard(hero));
-  defenderDiv.innerHTML = '';
-  defenderDiv.appendChild(createCard(defender));
-  // rite a function that creates a character card
-  // put hero back in herodiv, defender in defenderDiv
+  // WE need to update the DOM, empty the divs and reappend..IF game isn't over
+  if (hero.healthPoints >= 0 && defender.healthPoints >= 0) {
+    heroDiv.innerHTML = '';
+    heroDiv.appendChild(createCard(hero));
+    defenderDiv.innerHTML = '';
+    defenderDiv.appendChild(createCard(defender));
+  } else if (hero.healthPoints < 0) {
+    alert(
+      `you lose, rebel scum! ${hero.name} has ${
+        hero.healthPoints
+      } health remaining`
+    );
+  } else if (defender.healthPoints < 0) {
+    alert(`Yeahoooo ${defender.name} has been defeted`);
+    defender = characters[0];
+    defenderDiv.innerHTML = '';
+    defenderDiv.appendChild(createCard(defender));
+  }
 }
 
 // FIGHT!
